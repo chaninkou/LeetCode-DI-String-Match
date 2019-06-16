26fba6141b866e180cb257b7cba2738703cafc84
chaninkou <michaelkoulove@yahoo.com>
2019-06-15 23:28:03 -0700

    Finished another coding practice.

---
+++ LeetCode-DI-String-Match/src/matchString/Main.java
@@ -0,0 +1,18 @@
+package matchString;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		String input = "IDID";
+		
+		MatchStringSolution output = new MatchStringSolution();
+		
+		int[] answer = output.diStringMatch(input);
+		
+		System.out.println("Input: " + input);
+		System.out.println("Output: " + Arrays.toString(answer));
+	}
+	
+}
---
+++ LeetCode-DI-String-Match/src/matchString/MatchStringSolution.java
@@ -0,0 +1,23 @@
+package matchString;
+
+public class MatchStringSolution {
+    public int[] diStringMatch(String S) {
+        int size = S.length();
+        int left = 0;
+        int right = S.length();
+        
+        int[] array = new int[size + 1];
+            
+        for(int i = 0; i < size; i++){
+            if(S.charAt(i) == 'D'){
+                array[i] = right--;
+            } else{
+                array[i] = left++;
+            }
+        }
+        
+        array[size] = left;
+        
+        return array;
+    }
+}
